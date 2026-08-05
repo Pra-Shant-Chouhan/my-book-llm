afc53e20323c1e2530aaea16cb5e01cd773cf3d6
Prashant Chouhan <pchouhan122@gmail.com>
2026-08-05 15:03:04 +0530

    feat:prisma packages install and setup on backend

---
+++ server/prisma/migrations/20260805092901_test/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Test" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+
+    CONSTRAINT "Test_pkey" PRIMARY KEY ("id")
+);
