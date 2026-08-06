fd94e03fab772476e4bc7b11a6bf7b340b64e82e
Prashant Chouhan <pchouhan122@gmail.com>
2026-08-06 18:30:50 +0530

    feat: update CORS origin and restructure middleware setup in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,16 @@ import { auth } from "./lib/auth.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 import cors from "cors";
-// Mount express json middleware after Better Auth handler
-// or only apply it to routes that don't interact with Better Auth
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
+
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+        origin: process.env.CORS_ORIGIN || "http://localhost:3001", // 3000(hosted) || 3001
         credentials: true,
     }) 
 );
+
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.use(express.json());
@@ -23,6 +25,9 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.send("Health check passed!");
 });
+registerRoutes(app);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
